4cdb31b00921bb0bdb600d95d1c0e130259ded8a
Arika18 <sagarikt18@gmail.com>
2026-03-11 19:22:43 +0530

    add

---
+++ 3.java
@@ -0,0 +1,13 @@
+public class 00PSBannerApp {
+
+Run | Debug
+>
+public static void main(String[] args) {
+	System.out.println(String.join(" *** " , " *** " , "******"  ,   "  ***** "));
+    System.out.println(String.join("** **" , "** **" , "**    **" ,  " ** "));
+    System.out.println(String.join("** **" , "** **" , "**     **",  "** "));
+    System.out.println(String.join("** **" , "** **" , "**    **",   " *****"));
+    System.out.println(String.join("** **" , "** **" , "******" ,    "     **"));
+    System.out.println(String.join("** **" , "** **" , "**" ,        "**  **"));
+    System.out.println(String.join(" *** " , " *** " , "**" ,        " ***** "));
+}
