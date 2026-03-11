42f0b94075f717be3400210292743a9a0b461181
Arika18 <sagarikt18@gmail.com>
2026-03-11 20:03:38 +0530

    Add

---
+++ 5.java
@@ -0,0 +1,15 @@
+public class 00PSBannerApp {
+
+Run | Debug
+>
+public static void main(String[] args) 
+    String[] lines ={
+	String.join(" *** " , " *** " , "******"  ,   "  ***** "));
+    String.join("** **" , "** **" , "**    **" ,  " ** "));
+    String.join("** **" , "** **" , "**     **",  "** "));
+    String.join("** **" , "** **" , "**    **",   " *****"));
+    String.join("** **" , "** **" , "******" ,    "     **"));
+    String.join("** **" , "** **" , "**" ,        "**  **"));
+    String.join(" *** " , " *** " , "**" ,        " ***** "));
+	for (String line: lines) {
+        System.out.println(line);
