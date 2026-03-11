a6c49ddaa2260bd02d05be8f10df31ea2d70a088
Arika18 <sagarikt18@gmail.com>
2026-03-11 19:05:51 +0530

    Add

---
+++ 2.java
@@ -0,0 +1,13 @@
+public class 00PSBannerApp {
+
+Run | Debug
+>
+public static void main(String[] args) {
+	System.out.println(" *** " + "" + "  ***** ");
+    System.out.println("** **" + "" + " ** ");
+    System.out.println("** **" + "" + "** ");
+    System.out.println("** **" + "" + " *****");
+    System.out.println("** **" + "" + "     **");
+    System.out.println("** **" + "" + "**  **");
+    System.out.println(" *** " + "" + "***** ");
+}
